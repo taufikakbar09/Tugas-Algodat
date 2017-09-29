1ec475fcf631d7ab8bf3d4424682f5b9eba30997
Taufik Akbar <hp@DESKTOP-UODRVEK>
2017-09-29 22:41:42 +0700

    Tugas kelas algodat

---
+++ src/ArrayLinierList/Array2D.java
@@ -0,0 +1,25 @@
+package ArrayLinierList;
+
+public class Array2D {
+    public static Object[] ChangeLength(Object [] a, int n, int m, int newLength, int newLengthBaru){
+        if(n > newLength){
+            throw new IllegalArgumentException("Length is too small");
+        }
+        if(m > newLengthBaru){
+            throw new IllegalArgumentException("Length is too small");
+        }
+        if(a != null){
+            Object[][]copy = new Object [newLength][newLengthBaru];
+            for (int i = 0; i <a.length; i++){
+                final Object []row = (Object[]) a[i];
+                copy[i] = new Object[newLengthBaru];
+                System.arraycopy(row, 0, copy[i], 0, row.length);
+            }
+            return copy;
+        }
+        return null;
+    }
+    public static Object [][]ChangeLength(Object[][] a, int newLength, int newLengthBaru){
+        return(Object[][]) ChangeLength(a,a.length, a[0].length, newLength, newLengthBaru);
+    }
+}
